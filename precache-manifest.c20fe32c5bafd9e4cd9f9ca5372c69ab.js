d05cd93335650e2bfe26fc48f9839c2aeb0a56df
Owen McGrath <owenmcgrath85@gmail.com>
2024-02-08 18:44:31 -0600

    Updates

---
+++ precache-manifest.c20fe32c5bafd9e4cd9f9ca5372c69ab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da62e881dac742c80990e975b26db5fd",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "c2d886e503158b6f69b8",
+    "url": "/react-gh-pages/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "1f5929bab9e74fc2d234",
+    "url": "/react-gh-pages/static/js/2.a71b1702.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/react-gh-pages/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "c2d886e503158b6f69b8",
+    "url": "/react-gh-pages/static/js/main.a5509d43.chunk.js"
+  },
+  {
+    "revision": "5fe9ae10a285097a8de6",
+    "url": "/react-gh-pages/static/js/runtime~main.46c41bee.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/react-gh-pages/static/media/logo.06e73328.svg"
+  }
+]);
